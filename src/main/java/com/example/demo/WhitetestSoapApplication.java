aa63df4d4018f600998184c2e748f427e99d1a66
achrefbenabdallah <amriiheb.business@gmail.com>
2021-05-22 14:16:59 +0100

    ds_soap_partie1

---
+++ src/main/java/com/example/demo/WhitetestSoapApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhitetestSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhitetestSoapApplication.class, args);
+	}
+
+}
